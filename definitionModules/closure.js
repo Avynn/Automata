4d3a7efcd940fa6f631639bdaa483fb2ad94e3fd
Unknown <avynn@donaghe.com>
2018-06-27 21:00:46 -0700

    working on defining closure module

--- definitionModules/closure.js
+++ definitionModules/closure.js
@@ -2,21 +2,70 @@
 Closure definitions module.
 
 The aim of this module is to provide an object that represents a clousre
-in memory.  It will require a unique "toString" "method", in order to write it to code
-and a probably even more complex "fromString" "Method", This may cause problems in the future
-because there is a possibility that the rules for the writing of code in the standard usable
-by autmata may cause it to become too restrictive to write to code.
+in memory.  This is the core of the program and defines the behavior of
+execution of programs.
 */
 
+//constructor definition
 
-function closure (adjHead, next, func){
-    this.adjHead = adjHead;
+function closure (next, func){
+    /*
+    (closure next, any func)->obj<closure>
+
+    The next pointer is used to assemble lists
+    and the func pointer either points to a primitive
+    or another closure.
+    */
+
+    this.args = [];
     this.next = next;
     this.func = func;
 }
 
-closure.evaluate = function(args){
+//closure evaluator function and helpers.
+
+closure.evaluate = function(){
+    /*
+    (closure args)->closure
+
+    This function evaluates all parameters
+    and then evaluates the closure
+    */
+
+    //TODO: add check that arguments are continuous accross the list.
+
+    if(args[0]){
+        let curr = args.pop().evaluate();
+        this.args.push(curr);
+    } 
+
+    if(!(typeof(this.func) == typeof(closure))){
+        this.evaluated = this.func.evaluate()
+    }
     
+}
+
+closure.addArg = function(closure, pos){
+    /*
+    (closure closure, int pos)->null
+
+    This function adds an argument reference
+    to the closure in position pos
+    */
+
+    this.args[pos] = closure;
+}
+
+
+//primitives 
+
+
+
+//Utitlity/debugging functions bandaids for now
+
+
+closure.setNext = function(closure){
+    this.next = closure;
 }
 
 function head(closure){
@@ -24,7 +73,7 @@ function head(closure){
 }
 
 function rest(closure){
-    return closure.next;
+    return closure.adjHead;
 }
 
 
